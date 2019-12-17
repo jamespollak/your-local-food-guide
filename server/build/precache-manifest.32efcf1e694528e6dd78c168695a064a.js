807c6e0628140a0ffa251131a4bd0efc4453875c
Gijs <gijs@MacBook-Pro-di-Gebruiker.local>
2019-12-17 20:05:44 +0100

    push it real good

---
+++ server/build/precache-manifest.32efcf1e694528e6dd78c168695a064a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4c2183002f242d5e8876e4fdb1af6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8bb953648ab7245189a",
+    "url": "/static/css/main.a632ec8b.chunk.css"
+  },
+  {
+    "revision": "6289fce9503519d2ef99",
+    "url": "/static/js/2.6da84d5d.chunk.js"
+  },
+  {
+    "revision": "c8bb953648ab7245189a",
+    "url": "/static/js/main.2cf7440e.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "feb4881170b3d9527f33297554ce6e12",
+    "url": "/static/media/chef.feb48811.png"
+  }
+]);
