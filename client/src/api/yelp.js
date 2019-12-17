1130d684568b638aca0de8973ed7a1ab9be7335f
Gijs <gijs@MacBook-Pro-di-Gebruiker.local>
2019-12-17 20:21:00 +0100

    push it real good

--- client/src/api/yelp.js
+++ client/src/api/yelp.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// const YELP_API_KEY =
-//   "ETpD-WScgVpK-jsn29ZUxpHEVTM8mzHn3u2zISzVCzvuaBxK-Z5Qkfb7EjRllLLdXH0AJnbtpwQ0gqisdPTMrJtNf7vSKqr9jQxuGWzwmNlnjT6P15LpwKhVsFTiXXYx";
-
 const api = axios.create({
   baseURL: process.env.REACT_APP_YELP_URL,
   headers: {
